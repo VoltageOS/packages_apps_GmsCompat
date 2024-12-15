25a184d77d5a49c2c59a693a23bc33b054ac0993
Daniel Micay <daniel.micay@grapheneos.org>
2024-12-15 15:29:09 -0500

    config-holder: increment version to 152
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- config-holder/app/build.gradle.kts
+++ config-holder/app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
     defaultConfig {
         minSdk = 32
         targetSdk = 35
-        versionCode = 151
+        versionCode = 152
         versionName = versionCode.toString()
     }
 
