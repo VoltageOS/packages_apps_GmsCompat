e47aea9203e4594ff8475533430b073d5d469bdf
Daniel Micay <daniel.micay@grapheneos.org>
2026-06-10 02:03:57 -0400

    update build tools to 37.0.0
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- config-holder/app/build.gradle.kts
+++ config-holder/app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
     namespace = "app.grapheneos.gmscompat.config"
 
     compileSdk = 37
-    buildToolsVersion = "36.1.0"
+    buildToolsVersion = "37.0.0"
 
     defaultConfig {
         minSdk = 32
